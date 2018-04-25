oxteam/easyappointments

commit 355ddaeda498f7c3862b66db0217d11d36e95ee1

Integrated tab component to settings page.

src/assets/js/backend_settings.js
@@ -138,25 +138,18 @@ window.BackendSettings = window.BackendSettings || {};
          *
          * Change the visible tab contents.
          */
-        $('.tab').click(function() {
-            // Bootstrap has a bug with toggle buttons. Their toggle state is lost when the
-            // button is hidden or shown. Show before anything else we need to store the toggle
-            // and apply it whenever the user tab is clicked..
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (event) {
+            // Bootstrap has a bug with toggle buttons. Their toggle state is lost when the button is hidden or shown.
+            // Show before anything else we need to store the toggle and apply it whenever the user tab is clicked.
             var areNotificationsActive = $('#user-notifications').hasClass('active');
 
-            $(this).parent().find('.active').removeClass('active');
-            $(this).addClass('active');
-            $('.tab-content').hide();
+            var href = $(this).attr('href');
 
-            if ($(this).hasClass('general-tab')) {
-                $('#general').show();
+            if (href === '#general') {
                 settings = new SystemSettings();
-            } else if ($(this).hasClass('business-logic-tab')) {
-                $('#business-logic').show();
+            } else if (href === '#business-logic') {
                 settings = new SystemSettings();
-
-            } else if ($(this).hasClass('user-tab')) {
-                $('#user').show();
+            } else if (href === '#current-user') {
                 settings = new UserSettings();
 
                 // Apply toggle state to user notifications button.
@@ -165,8 +158,6 @@ window.BackendSettings = window.BackendSettings || {};
                 } else {
                     $('#user-notifications').removeClass('active');
                 }
-            } else if ($(this).hasClass('about-tab')) {
-                $('#about').show();
             }
 
             Backend.placeFooterToBottom();

commit 577a4c803ecec68a4c964bc165badffec48da30c

Fix issues in the 'settings' page when connected user is not admin: - The 'current user' tab, which is the first tab displayed for non admin users is blank  (regression caused by commit 355ddaeda498f7c3862b66db0217d11d36e95ee1). - Saving user settings fails becasue wrong setting helpers was used.

src/application/views/backend/settings.php
@@ -27,24 +27,26 @@
     });
 </script>
 
+<?php $first = ""; ?>
+
 <div id="settings-page" class="container-fluid">
     <ul class="nav nav-tabs" role="tablist">
         <?php if ($privileges[PRIV_SYSTEM_SETTINGS]['view'] == TRUE): ?>
-            <li role="presentation" class="active">
+            <li role="presentation" <?php if(empty($first)) { echo 'class="active"'; $first = 'general';} ?>>
                 <a href="#general" aria-controls="general" role="tab" data-toggle="tab"><?= lang('general') ?></a>
             </li>
         <?php endif ?>
         <?php if ($privileges[PRIV_SYSTEM_SETTINGS]['view'] == TRUE): ?>
-            <li role="presentation">
+            <li role="presentation" <?php if(empty($first)) { echo 'class="active"'; $first = 'business-logic';} ?>>
                 <a href="#business-logic" aria-controls="business-logic" role="tab" data-toggle="tab"><?= lang('business_logic') ?></a>
             </li>
         <?php endif ?>
         <?php if ($privileges[PRIV_USER_SETTINGS]['view'] == TRUE): ?>
-            <li role="presentation">
+            <li role="presentation" <?php if(empty($first)) { echo 'class="active"'; $first = 'current-user';} ?>>
                 <a href="#current-user" aria-controls="current-user" role="tab" data-toggle="tab"><?= lang('current_user') ?></a>
             </li>
         <?php endif ?>
-        <li role="presentation">
+        <li role="presentation" <?php if(empty($first)) { echo 'class="active"'; $first = 'about-app';} ?>>
             <a href="#about-app" aria-controls="about-app" role="tab" data-toggle="tab"><?= lang('about_app') ?></a>
         </li>
     </ul>
@@ -54,7 +56,7 @@
         <!-- GENERAL TAB -->
 
         <?php $hidden = ($privileges[PRIV_SYSTEM_SETTINGS]['view'] == TRUE) ? '' : 'hidden' ?>
-        <div role="tabpanel" class="tab-pane active <?= $hidden ?>" id="general">
+        <div role="tabpanel" class="tab-pane <?php if($first === 'general') echo 'active'; ?> <?= $hidden ?>" id="general">
             <form>
                 <fieldset>
                     <legend>
@@ -150,7 +152,7 @@
         <!-- BUSINESS LOGIC TAB -->
 
         <?php $hidden = ($privileges[PRIV_SYSTEM_SETTINGS]['view'] == TRUE) ? '' : 'hidden' ?>
-        <div role="tabpanel" class="tab-pane <?= $hidden ?>" id="business-logic">
+        <div role="tabpanel" class="tab-pane <?php if($first === 'business-logic') echo 'active'; ?> <?= $hidden ?>" id="business-logic">
             <form>
                 <fieldset>
                     <legend>
@@ -314,7 +316,7 @@
         <!-- CURRENT USER TAB -->
 
         <?php $hidden = ($privileges[PRIV_USER_SETTINGS]['view'] == TRUE) ? '' : 'hidden' ?>
-        <div role="tabpanel" class="tab-pane <?= $hidden ?>" id="current-user">
+        <div role="tabpanel" class="tab-pane <?php if($first === 'current-user') echo 'active'; ?> <?= $hidden ?>" id="current-user">
             <form>
                 <div class="row">
                     <fieldset class="col-xs-12 col-sm-6 personal-info-wrapper">
@@ -419,7 +421,7 @@
 
         <!-- ABOUT TAB -->
 
-        <div role="tabpanel" class="tab-pane" id="about-app">
+        <div role="tabpanel" class="tab-pane <?php if($first === 'general') echo 'about-app'; ?>" id="about-app">
             <h3>Easy!Appointments</h3>
 
             <p>

src/assets/js/backend_settings.js
@@ -97,8 +97,12 @@ window.BackendSettings = window.BackendSettings || {};
             $('#user-notifications').removeClass('active');
         }
 
-        // Set default settings helper.
-        settings = new SystemSettings();
+        if($('#current-user').hasClass('active')){
+            settings = new UserSettings();
+        }else{
+            // Set default settings helper.
+            settings = new SystemSettings();
+        }
 
         if (bindEventHandlers) {
             _bindEventHandlers();